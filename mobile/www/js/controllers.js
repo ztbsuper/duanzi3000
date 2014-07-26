d906363d72b20c50461384fbdb744121179f450f
tbzhang <tbzhang@thoughtworks.com>
2014-07-26 22:51:54 +0800

    fix mobile's publish

--- mobile/www/js/controllers.js
+++ mobile/www/js/controllers.js
@@ -10,12 +10,6 @@ app.controller('rankCtrl', function ($scope, API, favStorage) {
         .success(function (data, status) {
             $scope.count = data;
         });
-//    API.rank($scope.page)
-//        .success(function (data, status) {
-//            $scope.duanzis = data;
-//        })
-//        .error(function (data, status) {
-//        });
     $scope.fetchDuanzis = function () {
         $scope.page = $scope.page + 1;
         API.rank($scope.page)
@@ -29,7 +23,7 @@ app.controller('rankCtrl', function ($scope, API, favStorage) {
                 console.log("error");
             });
     };
-    $scope.$on('stateChangeSuccess', function() {
+    $scope.$on('stateChangeSuccess', function () {
         $scope.fetchDuanzis();
     });
     $scope.favAvailable = function (_id) {
@@ -46,42 +40,57 @@ app.controller('rankCtrl', function ($scope, API, favStorage) {
 });
 app.controller('favCtrl', function ($scope, API, favStorage, $window) {
     $scope.duanzis = [];
-    API.bulk(favStorage.getFav())
-        .success(function (data, status) {
-            $scope.duanzis = data;
-        })
-        .error(function (data, status) {
-            console.log("error");
-        });
-
+    if (favStorage.getFav().length > 0) {
+        API.bulk(favStorage.getFav())
+            .success(function (data, status) {
+                $scope.duanzis = data;
+            })
+            .error(function (data, status) {
+                console.log("error");
+            });
+    }
     $scope.removeFromFav = function (_id) {
         favStorage.removeFromFav(_id);
         $window.location.reload();
     };
-
 });
-app.controller('publishCtrl', function ($scope, $location) {
+
+app.controller('publishCtrl', function ($scope, $location, API, $ionicModal) {
+    $ionicModal.fromTemplateUrl("templates/thanks.html", {
+        scope: $scope
+    }).then(function (modal) {
+        $scope.modal = modal;
+    });
+
     $scope.loading = false;
+    $scope.duanzi = {
+        title: "", author: "", body: ""
+    };
     $scope.submit = function () {
         $scope.loading = true;
-        console.log($scope.author);
-        if ($scope.content.length == 0) {
+        console.log($scope.duanzi.title);
+        if ($scope.duanzi.body.length == 0) {
             $scope.loading = false;
         } else {
-            var content = strip_tags($scope.content, "<br/>").trim();
+            var content = [];
+            content.push(strip_tags($scope.duanzi.body, "<br/>").trim());
             var obj = {
-                title: $scope.title,
-                author: $scope.author,
+                title: $scope.duanzi.title,
+                author: $scope.duanzi.author,
                 duanzis: content
             };
             API.postCollection(obj)
                 .success(function (data, status) {
-                    loading.close();
-                    $location.path("/rank");
+                    $scope.loading = false;
+                    $scope.modal.show();
                 })
                 .error(function (data, status) {
-                    loading.close();
                 });
+
         }
+    };
+    $scope.closeModal = function () {
+        $scope.modal.hide();
+        $location.path('/rank');
     }
 });
