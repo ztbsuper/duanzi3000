7e52bf14e43be8c5a425fd6aaed389a1b8f53990
tbzhang <tbzhang@thoughtworks.com>
2014-07-28 15:58:35 +0800

    make shell work

--- client/deploy_aws_s3.sh
+++ client/deploy_aws_s3.sh
@@ -1,3 +1,3 @@
 grunt build && cd ./dist
-aws s3 rm s3://3kduanzi.com/ --recursive
+aws s3 rm s3://3kduanzi.com/ --recursive  --region ap-northeast-1
 aws s3 cp . s3://3kduanzi.com/ --recursive --region ap-northeast-1 --acl public-read
